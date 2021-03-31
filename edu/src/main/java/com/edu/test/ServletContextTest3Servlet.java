abad9e8fd52c967fbb044d1e3b323efbf108ac50
Administrator <Administrator@DESKTOP-FU2AS1G>
2021-03-31 16:43:33 +0900

    edu/servlet

---
+++ edu/src/main/java/com/edu/test/ServletContextTest3Servlet.java
@@ -0,0 +1,28 @@
+package com.edu.test;
+
+import java.io.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+@WebServlet("/context3")
+public class ServletContextTest3Servlet extends HttpServlet {
+
+	public void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = resp.getWriter();
+		ServletContext sc=this.getServletContext();
+		ShareObject obj1=new ShareObject();
+		obj1.setCount(100);
+		obj1.setStr("객체공유테스트-1");
+		sc.setAttribute("data1", obj1);
+		
+		ShareObject obj2=new ShareObject();
+		obj2.setCount(100);
+		obj2.setStr("객체공유테스트-2");
+		sc.setAttribute("data2", obj2);
+		
+		out.print("ServlertContext 객체에 데이터등록을 하였습니다!");
+		out.close();
+		
+	}
+}
